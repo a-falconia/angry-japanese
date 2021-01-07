40690c8d7c81ee56a4181507da9124df124a5817
LaineyPrice <anna.falconia@gmail.com>
2021-01-07 23:02:26 +0300

    some more logic

--- words.js
+++ words.js
@@ -1,5 +1,3 @@
-console.log("hi!!")
-
 var dict = [
     ["おはようございます", "Ohayou gozaimasu", "good morning"],
     ["こんにちは", "Konnichiwa", "hello"],
@@ -9,16 +7,52 @@ var dict = [
     ["すみません", "Sumimasen", "sorry"],
     ["ごめんなさい", "Gomennasai", "sorry"],
     ["はい", "Hai", "yes"],
-    ["いいえ", "Iie", "no"]
-]
-
-var randword = Math.floor(Math.random() * dict.length);
-var randCard = dict[randword];
-console.log(randword);
-console.log(randCard);
-
+    ["いいえ", "iie", "no"]
+];
 const quizKana = document.querySelector("#kana");
 const quizRomaji = document.querySelector("#romaji");
 
-quizKana.textContent = randCard[0];
-quizRomaji.textContent = randCard[1];
+const card = document.querySelector("#flashcard");
+const ansForm = document.querySelector("#ansForm");
+const answer = document.querySelector("#answer");
+const score = document.querySelector("#score");
+const feedback = document.querySelector("#angryFeedback");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getCard = function () {
+
+    var randword = Math.floor(Math.random() * dict.length);
+    var randCard = dict[randword];
+    quizKana.textContent = randCard[0];
+    quizRomaji.textContent = randCard[1];
+    return randCard[2]; //TODO: Костыль, реализовать через класс карточки?
+}
+
+var ans = getCard(); //TODO: Костыль, реализовать через класс карточки?
+var points = 0;
+
+ansForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    if (answer.value == ans) {
+
+        points += 10;
+        await sleep(500);
+        feedback.textContent = "ok... for now";
+        feedback.style.color = 'green';
+    }
+    else {
+
+        points = 0;
+        await sleep(500);
+
+        feedback.textContent = "Wrong, you idiot";
+        feedback.style.color = 'red';
+
+    }
+    answer.value = '';
+    ans = getCard();
+    score.textContent = `Your Score: ${points}`
+});
