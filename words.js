63c92d2d82c719d9997b2accde491a7faacade50
LaineyPrice <anna.falconia@gmail.com>
2021-01-12 22:52:01 +0300

    deck mechanism

--- words.js
+++ words.js
@@ -1,26 +1,3 @@
-var dict = [
-    ["おはようございます", "Ohayou gozaimasu", "good morning"],
-    ["こんにちは", "Konnichiwa", "hello"],
-    ["こんばんは", "Konbanwa", "good evening"],
-    ["おやすみなさい", "Oyasuminasai", "goodnight"],
-    ["ありがとうございます", "Arigatou gozaimasu", "thank you"],
-    ["すみません", "Sumimasen", "sorry"],
-    ["ごめんなさい", "Gomennasai", "sorry"],
-    ["はい", "Hai", "yes"],
-    ["いいえ", "iie", "no"]
-];
-const quizKana = document.querySelector("#kana");
-const quizRomaji = document.querySelector("#romaji");
-
-const card = document.querySelector("#flashcard");
-const ansForm = document.querySelector("#ansForm");
-const answer = document.querySelector("#answer");
-const score = document.querySelector("#score");
-const feedback = document.querySelector("#angryFeedback");
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 class Flashcard {
 
@@ -31,38 +8,68 @@ class Flashcard {
     }
 
 }
+
+var dict = [
+    // new Flashcard(["おはようございます", "Ohayou gozaimasu", "good morning"]),
+    // new Flashcard(["こんばんは", "Konbanwa", "good evening"]),
+    // new Flashcard(["こんにちは", "Konnichiwa", "hello"]),
+    // new Flashcard(["おやすみなさい", "Oyasuminasai", "goodnight"]),
+    // new Flashcard(["ありがとうございます", "Arigatou gozaimasu", "thank you"]),
+    // new Flashcard(["すみません", "Sumimasen", "sorry"]),
+    new Flashcard(["ごめんなさい", "Gomennasai", "sorry"]),
+    new Flashcard(["はい", "Hai", "yes"]),
+    new Flashcard(["いいえ", "iie", "no"]),
+];
+const quizKana = document.querySelector("#kana");
+const quizRomaji = document.querySelector("#romaji");
+const card = document.querySelector("#flashcard");
+const ansForm = document.querySelector("#ansForm");
+const answer = document.querySelector("#answer");
+const score = document.querySelector("#score");
+const feedback = document.querySelector("#angryFeedback");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const getCard = function () {
 
     var randword = Math.floor(Math.random() * dict.length);
     var randCard = dict[randword];
-    quizKana.textContent = randCard[0];
-    quizRomaji.textContent = randCard[1];
-    return randCard[2]; //TODO: Костыль, реализовать через класс карточки?
+    quizKana.textContent = randCard.kana;
+    quizRomaji.textContent = randCard.romaji;
+    return randCard;
 }
 
-var ans = getCard(); //TODO: Костыль, реализовать через класс карточки?
+var flashcard = getCard();
 var points = 0;
 
 ansForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    if (answer.value == ans) {
+    if (answer.value == flashcard.english) {
 
         points += 10;
         await sleep(500);
         feedback.textContent = "ok... for now";
         feedback.style.color = 'green';
+        dict.splice(dict.indexOf(flashcard), 1);
+        console.log(dict)
     }
     else {
 
         points = 0;
         await sleep(500);
-
         feedback.textContent = "Wrong, you idiot";
         feedback.style.color = 'red';
-
+        console.log(dict);
     }
     answer.value = '';
-    ans = getCard();
-    score.textContent = `Your Score: ${points}`
+    score.textContent = `Your Score: ${points}`;
+    if (dict.length !== 0) { flashcard = getCard(); }
+    else {
+        quizKana.textContent = "А всё а больше нету карточек";
+        quizRomaji.textContent = '';
+    } //Getting new card
+
 });
